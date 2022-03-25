400c066fd13a191702f2f9b8f661193d42a9779d
root <sajjan.arun@gmail.com>
2022-03-25 10:44:53 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -2,19 +2,19 @@ module "vpc" {
     source  = "terraform-google-modules/network/google"
     version = "~> 4.0"
 
-    project_id   = var.project
-    network_name = "gaurav-network"
+    project_id   = "<PROJECT ID>"
+    network_name = "example-vpc"
     routing_mode = "GLOBAL"
 
     subnets = [
         {
-            subnet_name           = "gaurav-subnet"
-            subnet_ip             = "10.100.10.0/24"
-            subnet_region         = var.region
+            subnet_name           = "subnet-01"
+            subnet_ip             = "10.10.10.0/24"
+            subnet_region         = "us-west1"
         },
         {
             subnet_name           = "subnet-02"
-            subnet_ip             = "10.100.20.0/24"
+            subnet_ip             = "10.10.20.0/24"
             subnet_region         = "us-west1"
             subnet_private_access = "true"
             subnet_flow_logs      = "true"
@@ -22,7 +22,7 @@ module "vpc" {
         },
         {
             subnet_name               = "subnet-03"
-            subnet_ip                 = "10.100.30.0/24"
+            subnet_ip                 = "10.10.30.0/24"
             subnet_region             = "us-west1"
             subnet_flow_logs          = "true"
             subnet_flow_logs_interval = "INTERVAL_10_MIN"
